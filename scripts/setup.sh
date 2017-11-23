98ff0569d7521347d3f54c96b19f81c272daf588
shirakiya <shirakiya.pv@gmail.com>
2017-11-23 21:24:58 +0900

    test CodeDeploy

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+/root/local/python-3.6.2/bin/pip install -f wheelhouse --no-index -r requirements.txt
+
 if [ $DEPLOYMENT_GROUP_NAME = 'jaaxman-job' ]; then
   /root/local/python-3.6.2/bin/python manage.py migrate
 fi
