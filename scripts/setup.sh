123d0c16ca5db23d2c100655531d317c42cf7c5b
shirakiya <shirakiya.pv@gmail.com>
2017-12-02 19:12:36 +0900

    workaround to install mysqliclient for ubuntu

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,10 @@ export $(cut -d = -f 1 ${ENVFILE})
 # setup app
 cd /root/jaaxman
 /root/local/python-3.6.2/bin/pip install -f wheelhouse --no-index -r requirements.txt
+# workaround
+# ref.) https://stackoverflow.com/questions/36796167/upgraded-to-ubuntu-16-04-now-mysql-python-dependencies-are-broken
+/root/local/python-3.6.2/bin/pip uninstall mysqlclient
+/root/local/python-3.6.2/bin/pip install mysqlclient==1.3.12
 
 if [ $DEPLOYMENT_GROUP_NAME = 'jaaxman-job' ]; then
   /root/local/python-3.6.2/bin/python manage.py migrate
