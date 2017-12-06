be0db33edec7f94e381b089483766d9115d67990
shirakiya <shirakiya.pv@gmail.com>
2017-12-06 21:56:51 +0900

    fix img path

--- src/js/utils/statics.js
+++ src/js/utils/statics.js
@@ -1,6 +1,6 @@
 function getStaticPath() {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://s3-ap-northeast-1.amazonaws.com/jaaxman-production-public/';
+    return 'https://s3-ap-northeast-1.amazonaws.com/jaaxman-production-public';
   } else {
     return '/static';
   }
