0d99162673bd9c574b853ba17a34eb4f4c435ec1
shirakiya <shirakiya.pv@gmail.com>
2018-02-25 21:10:24 +0900

    disabled executing fetchrss via request

--- jaaxman/settings.py
+++ jaaxman/settings.py
@@ -69,8 +69,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'app.middleware.authorization.AuthorizationMiddleware',
 ]
 
 # AuthorizationMiddleware
