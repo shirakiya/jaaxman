1a9bcb8fb125004b15009328f7aa96bcf9546725
shirakiya <shirakiya.pv@gmail.com>
2020-01-24 01:37:16 +0900

    update npm modules

--- frontend/src/js/components/navbar.vue
+++ frontend/src/js/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+<nav class="navbar is-fixed-top has-shadow" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="/">
       <img :src="brandImage" alt="Jaaxman"></img>
@@ -76,8 +76,8 @@ export default {
 
 <style lang="scss" scoped>
 .navbar {
-  border-bottom: 1px solid lightgray;
-  box-shadow: 0 1px 3px rgba(10, 10, 10, 0.1);
+  // border-bottom: 1px solid lightgray;
+  // box-shadow: 0 1px 3px rgba(10, 10, 10, 0.1);
   z-index: 20;
 
   .navbar-menu {
--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,16 +1,24 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge.smart(common, {
   mode: 'production',
   plugins: [
-    new UglifyJsPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name]-[hash].css',
     }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
   module: {
     rules: [
       {
